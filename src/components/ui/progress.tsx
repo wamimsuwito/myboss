b8ba76c14d616fbbb2948603678ff7496e8b96d7
Wamin Suwito <waminsuwito66@gmail.com>
2025-08-19 15:51:31 +0000

    KENAPA ANIMASI SEMEN TIDAK SELESAI JUGA

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -19,7 +20,7 @@ const Progress = React.forwardRef<
   >
     <ProgressPrimitive.Indicator
       className={cn("h-full w-full flex-1 bg-primary transition-all", indicatorClassName)}
-      style={{ transform: `translateY(${100 - (value || 0)}%)` }}
+      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
 ))
