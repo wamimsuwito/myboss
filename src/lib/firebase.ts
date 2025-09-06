2811f5bcabec05f322c11fd1ec2a21295fac90fb
Wamin Suwito <waminsuwito66@gmail.com>
2025-09-06 01:55:57 +0000

    saya ingin mengubah alamat setting proyek ini menggunakan ini <script ty

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,23 +1,29 @@
-
 // This file is now configured for a real Firebase project.
 import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAnalytics, isSupported } from "firebase/analytics";
 import { getFirestore, collection, getDocs, getDoc, setDoc, doc, addDoc, updateDoc, deleteDoc, serverTimestamp, onSnapshot, Timestamp, query, where, limit, orderBy, runTransaction, FieldValue, increment, writeBatch } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDQHD5hWDvY_Jp7kTsvOJ4Yei_fRYVgA3Y",
-  authDomain: "batchscale-monitor.firebaseapp.com",
-  projectId: "batchscale-monitor",
-  storageBucket: "batchscale-monitor.firebasestorage.app",
-  messagingSenderId: "425726816093",
-  appId: "1:425726816093:web:93af9e102298d4d07082b4"
+    apiKey: "AIzaSyDf7LyBES-QM4GaqxCQIIjMFlxKRtXyQvg",
+    authDomain: "mymanager-a17ba.firebaseapp.com",
+    projectId: "mymanager-a17ba",
+    storageBucket: "mymanager-a17ba.firebasestorage.app",
+    messagingSenderId: "1081214026393",
+    appId: "1:1081214026393:web:401a81a833c3ac1dc5ae08",
+    measurementId: "G-Y460R3EJ4V"
 };
 
 // Initialize Firebase.
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
 
+// Initialize Analytics if supported
+const analytics = isSupported().then(yes => yes ? getAnalytics(app) : null);
+
+
 export { 
     db,
+    analytics,
     collection, 
     doc,
     updateDoc,
